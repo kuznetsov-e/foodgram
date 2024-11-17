d94d902f885138b8ebaab488361843609cdca55c
Evgeny Kuznetsov <honourable.kensei@gmail.com>
2024-11-17 11:35:26 +0800

    minor sorting fixes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
-from .views import IngredientViewset, RecipeViewset, TagViewset
+from .views import IngredientViewset, RecipeViewset, TagViewset, UserViewSet
 
 router = DefaultRouter()
 
